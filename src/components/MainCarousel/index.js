c8ad97ef169e81301ad3c78608edb42ab0a1e795
Manuel Salgado <mest.sal18@gmail.com>
2019-01-10 22:58:05 -0500

    Added Carousel component

---
+++ src/components/MainCarousel/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
+import { Image } from './styles'
+import slideOne from '../../assets/images/slide-1.png'
+import slideTwo from '../../assets/images/slide-2.png'
+import slideThree from '../../assets/images/slide-3.png'
+import slideFour from '../../assets/images/slide-4.png'
+
+const MainCarousel = () => {
+    return (
+			<Carousel interval={8000}>
+				<Carousel.Item>
+					<Image alt="slide one" src={slideOne} />
+				</Carousel.Item>
+				<Carousel.Item>
+						<Image alt="slide two" src={slideTwo}/>
+				</Carousel.Item>
+				<Carousel.Item>
+						<Image alt="slide three" src={slideThree} />
+				</Carousel.Item>
+				<Carousel.Item>
+						<Image alt="slide four" src={slideFour} />
+				</Carousel.Item>
+			</Carousel>
+    )
+}
+
+export default MainCarousel
---
+++ src/components/MainCarousel/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Image = styled.img`
+	transition: 300ms;
+	@media (max-width: 1024px) {
+			width: 160%!important;
+			max-width: none!important;
+			position: relative;
+			left: -50%;
+		}
+	@media (max-width: 870px) {
+			width: 200%!important;
+			max-width: none!important;
+			position: relative;
+			left: -50%;
+		}
+	@media (max-width: 600px) {
+		width:300%!important;
+		max-width: none!important;
+		position: relative;
+		left: -50%;
+	}
+`
